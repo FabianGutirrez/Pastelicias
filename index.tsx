3a37a7219c2ef35661d7dbc86a6e8f2d8c23919a
FabianGutirrez <151792223+FabianGutirrez@users.noreply.github.com>
2026-03-31 00:10:37 +0000

    ordern

--- index.tsx
+++ index.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
